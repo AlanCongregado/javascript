11191d8d3c440429c85ecec7a7d5402ee7675cab
AlanCongregado <alan.congregado@hotmail.com>
2022-07-11 23:17:27 -0300

    aplicacion interactiva e-commerce de articulos de gym

--- js/script.js
+++ js/script.js
@@ -9,7 +9,6 @@ return sueldoTotal;
 
 sueldoVendedor() */
 
-
 /* programa para determinar si un alumno "promociona","aprueba" o "reprueba" */
 
 /* let nota = parseFloat(prompt("Ingrese la nota del alumno"))
@@ -46,23 +45,71 @@ while (numero <= 50) {
 } */
 
 /* Desafio 2 */
-
-let pinUsuario = '1287';
+/* Un login para inicio de sesion a un e-commerce de productos de gimnasio/ crossfit */
+let usuario = prompt("Ingrese su nombre de usuario");
+let pinUsuario = "1287";
 let ingresar = false;
 
-for(let i = 2; i >= 0; i--){
-    let pinIngresado = prompt("ingrese su pin");
-    if(pinIngresado == pinUsuario){
-        alert('Login exitoso');
-        ingresar = true;
-        break
-    }else {
-        alert('Error. te quedan ' + i + ' intentos');
+function saludar(usuario) {
+  let saludo = "Hola " + usuario + " bienvenido";
+  return saludo;
+}
+
+for (let i = 2; i >= 0; i--) {
+  let pinIngresado = prompt("ingrese su pin");
+  if (pinIngresado == pinUsuario) {
+    alert(saludar(usuario));
+    ingresar = true;
+    break;
+  } else {
+    alert("Error. te quedan " + i + " intentos");
+  }
+}
+
+if (ingresar == true) {
+  let opcion = prompt(
+    "que vas a comprar? \n1.disco de 5kg=$200 \n2.disco de 10kg=$300 \n3.disco de 15kg=$400m \n4.disco de 20kg=$500. \nsi queres salir presioná x."
+  );
+  let cantidad = parseInt(prompt("Cuantos discos vas a llevar?"));
+  let disco5 = 200;
+  let disco10 = 300;
+  let disco15 = 400;
+  let disco20 = 500;
+  let iva = 1.21;
+  function total(a, b, c) {
+    let importeTotal = a * b * c;
+    return importeTotal;
+  }
+  while (opcion != "x" || opcion != "X") {
+    switch (opcion) {
+      case "1":
+        alert(
+          "El total de tu compra con IVA es de: $" +
+            total(cantidad, disco5, iva)
+        );
+        break;
+      case "2":
+        alert(
+          "El total de tu compra con IVA es de: $" +
+            total(cantidad, disco10, iva)
+        );
+        break;
+      case "3":
+        alert(
+          "El total de tu compra con IVA es de: $" +
+            total(cantidad, disco15, iva)
+        );
+        break;
+      case "4":
+        alert(
+          "El total de tu compra con IVA es de: $" +
+            total(cantidad, disco20, iva)
+        );
+        break;
+
+      default:
+        alert("opcion no válida");
+        break;
     }
+  }
 }
-
-if(ingresar){
-    
-}
-
-
