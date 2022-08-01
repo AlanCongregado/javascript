df0223eae80f9402a3ecbfdab8b3acb20558f4a9
AlanCongregado <alan.congregado@hotmail.com>
2022-08-01 00:46:09 -0300

    funcionalidad del carrito

--- desafio 2/js/script.js
+++ desafio 2/js/script.js
@@ -1,81 +1,95 @@
-class Producto {
-  constructor(nombre, precio, marca) {
-    this.nombre = nombre;
-    this.precio = precio;
-    this.marca = marca;
-  }
-}
-let usuario = prompt("Ingrese su nombre de usuario");
-let pinUsuario = "1287";
-let ingresar = false;
+const productAddButtons = document.querySelectorAll(".product-add");
+productAddButtons.forEach((productAddButton) => {
+  productAddButton.addEventListener("click", addToCart);
+});
 
-function saludar(usuario) {
-  let saludo = "Hola " + usuario + " bienvenido";
-  return saludo;
-}
-
-for (let i = 2; i >= 0; i--) {
-  let pinIngresado = prompt("ingrese su pin");
-  if (pinIngresado == pinUsuario) {
-    alert(saludar(usuario));
-    ingresar = true;
-    break;
-  } else {
-    alert("Error. te quedan " + i + " intentos");
-  }
-}
-
-if (ingresar == true) {
-  let opcion = prompt(
-    "que vas a comprar? \n1.disco de 5kg=$200 \n2.disco de 10kg=$300 \n3.disco de 15kg=$400m \n4.disco de 20kg=$500. \nsi queres salir presioná x."
-  );
-  let cantidad = parseInt(prompt("Cuantos discos vas a llevar?"));
-  let disco5 = 200;
-  let disco10 = 300;
-  let disco15 = 400;
-  let disco20 = 500;
-  let iva = 1.21;
-  function total(a, b, c) {
-    let importeTotal = a * b * c;
-    return importeTotal;
-  }
-}
-
-const productos = [
-  new Producto("disco 5kg", 2000, "strong"),
-  new Producto("disco 10kg", 3000, "strong"),
-  new Producto("disco 15kg", 4000, "G-fitness"),
-  new Producto("disco 20kg", 5000, "G-fitness"),
-];
-
-const carrito = [];
-
-let criterio = parseInt(
-  prompt("Elegí el criterio deseado:\n1 - Precio (A a Z) \n2 - Precio (Z a A)")
+const shoppingCartItemsContainer = document.querySelector(
+  ".shoppingCartItemsContainer"
 );
 
-function ordernado(criterio, array) {
-  let arrayOrdenado = array.slice(0);
+function addToCart(e) {
+  const button = e.target;
+  const item = button.closest(".product-container");
 
-  switch (criterio) {
-    case 1:
-      let nombreAscendente = arrayOrdenado.sort((a, b) =>
-        a.precio.localeCompare(b.precio)
-      );
-      return nombreAscendente;
-    case 2:
-      let nombreDescendente = arrayOrdenado.sort((a, b) =>
-        b.precio.localeCompare(a.precio)
-      );
-      return nombreDescendente;
-    default:
-      alert("Criterio inválido");
-      break;
-  }
+  const itemTitle = item.querySelector(".product-name").textContent;
+  const itemPrice = item.querySelector(".product-price").textContent;
+  const itemImage = item.querySelector(".product-img").src;
+
+  addItemToShoppingCart(itemTitle, itemPrice, itemImage);
 }
 
-const addProduct = document.getElementsByClassName(".product-add");
+function addItemToShoppingCart(itemTitle, itemPrice, itemImage) {
+  const shoppingCartRow = document.createElement("div");
+  const shoppingCartContent = `
+  <div class="row shoppingCartItem">
+            <div class="col-6">
+                <div class="shopping-cart-item d-flex align-items-center h-100 border-bottom pb-2 pt-3">
+                    <img src=${itemImage} class="shopping-cart-image">
+                    <h6 class="shopping-cart-item-title shoppingCartItemTitle text-truncate ml-3 mb-0">${itemTitle}</h6>
+                </div>
+            </div>
+            <div class="col-2">
+                <div class="shopping-cart-price d-flex align-items-center h-100 border-bottom pb-2 pt-3">
+                    <p class="item-price mb-0 shoppingCartItemPrice">${itemPrice}</p>
+                </div>
+            </div>
+            <div class="col-4">
+                <div
+                    class="shopping-cart-quantity d-flex justify-content-between align-items-center h-100 border-bottom pb-2 pt-3">
+                    <input class="shopping-cart-quantity-input shoppingCartItemQuantity" type="number"
+                        value="1">
+                    <button class="btn btn-danger buttonDelete" type="button">X</button>
+                </div>
+            </div>
+        </div>
+  `;
+  shoppingCartRow.innerHTML = shoppingCartContent;
+  shoppingCartItemsContainer.append(shoppingCartRow);
 
-addProduct.addEventListener("click", () => {
-  console.log("Le diste click");
-});
+  shoppingCartRow
+    .querySelector(".buttonDelete")
+    .addEventListener("click", removeShoppingCartItem);
+
+  shoppingCartRow
+    .querySelector(".shoppingCartItemQuantity")
+    .addEventListener("change", quantityChanged);
+
+  updateCartTotal();
+}
+
+function updateCartTotal() {
+  let total = 0;
+  const shoppingCartTotal = document.querySelector(".shoppingCartTotal");
+  const shoppingCartItems = document.querySelectorAll(".shoppingCartItem");
+
+  shoppingCartItems.forEach((shoppingCartItem) => {
+    const shoppingCartItemPriceElement = shoppingCartItem.querySelector(
+      ".shoppingCartItemPrice"
+    );
+    const shoppingCartItemPrice = Number(
+      shoppingCartItemPriceElement.textContent.replace("$", "")
+    );
+    const shoppingCartItemQuantityElement = shoppingCartItem.querySelector(
+      ".shoppingCartItemQuantity"
+    );
+    const shoppingCartItemQuantity = Number(
+      shoppingCartItemQuantityElement.value
+    );
+    total = total + shoppingCartItemPrice * shoppingCartItemQuantity;
+  });
+  shoppingCartTotal.innerHTML = `$${total.toFixed(2)}`;
+}
+
+function removeShoppingCartItem(e) {
+  const buttonClicked = e.target;
+  buttonClicked.closest(".shoppingCartItem").remove();
+  updateCartTotal();
+}
+
+function quantityChanged(e) {
+  const input = e.target;
+  if (input.value <= 0) {
+    input.value = 1;
+  }
+  updateCartTotal();
+}
